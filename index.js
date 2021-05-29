478af2b796aab4bcba3098fcfe2fc3545b04b257
Jessica Antunes <jessantunes@protonmail.com>
2021-05-29 15:06:03 -0400

    commit tree 819ca410dfd78708be6114684293bf886fdf27c4

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name);
+};
+
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name);
+};
+
+function destructivelyRemoveLastDriver() {
+    drivers.pop();
+};
+
+function destructivelyRemoveFirstDriver() {
+    drivers.shift();
+};
+
+function appendDriver(name) {
+    const array = [drivers[0], drivers[1], drivers[2], name];
+    return array;
+};
+
+function prependDriver(name) {
+    let array2 = [name, drivers[0], drivers[1], drivers[2]];
+    return array2;
+};
+
+function removeLastDriver() {
+    let newArray = [drivers[0], drivers[1]];
+    return newArray;
+};
+
+function removeFirstDriver() {
+    let newArray2 = [drivers[1], drivers[2]];
+    return newArray2;
+};
